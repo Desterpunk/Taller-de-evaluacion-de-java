b66f4fab034285868f1c46830a26ba086cb488f1
Jhon Serna <jhonalfaber@gmail.com>
2021-09-07 00:33:47 -0500

    Creacion y uso del metodo AreaOfCircle en MathematicFunctions

--- src/main/java/com/sofka/MathematicFunctions.java
+++ src/main/java/com/sofka/MathematicFunctions.java
@@ -6,8 +6,16 @@ public class MathematicFunctions {
             System.out.println(num1 + " Is bigger than " + num2);
         } else if(num1 < num2){
             System.out.println(num1 + " Is less than " + num2);
-        } else if(num1 = num2){
+        } else if(num1 == num2){
             System.out.println(num1 + " Is equals than " + num2);
+        }
+    }
+
+    public static void AreaOfCircle(double radius){
+        if (radius<0){
+            System.out.println("Think about it");
+        } else if(radius >= 0) {
+            System.out.println("The area of a circle with " + radius + " mm of radius is " + String.format("%.2f", Math.PI * Math.pow(radius, 2)) + "(mm)^2");
         }
     }
 }
--- src/main/java/com/sofka/Menu.java
+++ src/main/java/com/sofka/Menu.java
@@ -1,7 +1,56 @@
 package com.sofka;
 
+import java.io.*;
+
 public class Menu {
     public static void main(String[] args) {
+        boolean running = true;
+        while(running){
+            System.out.println("Welcome!, Select one operation: ");
+            System.out.println("1) Is this number greater,less, or equal?.");
+            System.out.println("2) Area of a circle.");
+            System.out.println("3) 21% of Iva, life is wonderful");
+            System.out.println("0) Exit");
+            InputText opcion = new InputText(" your option");
+            switch (opcion.getText()){
+                case "1":
+                    InputText num1IsBigger = new InputText("num1");
+                    InputText num2IsBigger = new InputText("num2");
+                    MathematicFunctions.IsBigger(num1IsBigger.getDouble(),num2IsBigger.getDouble());
+                    break;
+                case "2":
+                    InputText radiusCircle1 = new InputText("radius (mm)");
+                    MathematicFunctions.AreaOfCircle(radiusCircle1.getDouble());
+                    break;
+                case "0":
+                    System.out.println("Have a good day");
+                    running = false;
+                    break;
+            }
+
+        }
 
     }
+}
+
+class InputText {
+    String text = "";
+
+    public InputText(String requiredInfo) {
+        System.out.println("Please enter " + requiredInfo + ":");
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        try {
+            text = bufferedReader.readLine();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public String getText() {
+        return text;
+    }
+    public double getDouble() {
+        return Double.parseDouble(text);
+    }
+
 }
